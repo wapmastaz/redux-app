4cc604ea8027650e34915cced3d736532a428b3e
wapmastaz <demolatheophilus8@gmail.com>
2024-06-06 17:02:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './state/store';
+import {
+	decrement,
+	increment,
+	incrementByAmount,
+  incrementAsync
+} from './state/counter/counterSlice';
+
+function App() {
+	const count = useSelector((state: RootState) => state.counter.value);
+	const dispatch = useDispatch<AppDispatch>();
+
+	return (
+		<>
+			<h1>Hello Redux</h1>
+			<div className='card'>
+				<h2>count is {count}</h2>
+
+				<button onClick={() => dispatch(incrementAsync(10))}>Increment</button>
+
+				<button onClick={() => dispatch(decrement())}>Decrement</button>
+
+				<button onClick={() => dispatch(incrementByAmount(20))}>
+					Increment by 20
+				</button>
+			</div>
+			<p className='read-the-docs'>Demmy</p>
+		</>
+	);
+}
+
+export default App;
